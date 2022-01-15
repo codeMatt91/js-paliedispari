6690ef966da43f6925de7575fe935884c4c846cd
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-15 19:15:47 +0100

    Added second exercise even or shots

---
+++ js/number.js
@@ -0,0 +1,22 @@
+/*
+
+Pari e Dispari
+L’ utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione)
+Dichiariamo chi ha vinto.
+
+1- chiediamo all'utente se pari o dispari
+2- chiediamo di inserire un numero
+3- generiamo un numero da 1 a 5 per il pc
+4- sommiamo il risultato 
+5- usaimo una funzione per dire se il risultato è pari o dispari
+6- dichiariamo chi ha vinto
+
+*/
+// !RECUPERIAMO DAL DOMDOVE SCRIVERE
+
+const evenOrShots = document.getElementById('palindrom');
+const generate = document.getElementById('generate');
+const response = document.getElementById('response');
